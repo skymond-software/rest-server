cf5a68accf2ad7b7d46c89e2924404b2a9f4dc31
James Card <jbcard@gmail.com>
2024-07-14 14:46:01 -0700

    Adding root makefiles to C.next.

---
+++ lib/cnext/include.mk
@@ -0,0 +1,50 @@
+MKDIR       := mkdir -p
+SRC_DIR     := src
+INCLUDE_DIR := include
+FLAGS       := -rdynamic
+REMOVE      := rm -rf
+ARCHIVE     := ar rcs
+
+OBJ_FILES := \
+    $(OBJ_DIR)/Coroutines.o \
+    $(OBJ_DIR)/DataTypes.o \
+    $(OBJ_DIR)/Dictionary.o \
+    $(OBJ_DIR)/DirectoryLib.o \
+    $(OBJ_DIR)/Glibc27CompatLib.o \
+    $(OBJ_DIR)/HashTable.o \
+    $(OBJ_DIR)/List.o \
+    $(OBJ_DIR)/PosixCThreads.o \
+    $(OBJ_DIR)/PosixProcesses.o \
+    $(OBJ_DIR)/Queue.o \
+    $(OBJ_DIR)/RandomLib.o \
+    $(OBJ_DIR)/RedBlackTree.o \
+    $(OBJ_DIR)/Scope.o \
+    $(OBJ_DIR)/Sockets.o \
+    $(OBJ_DIR)/Stack.o \
+    $(OBJ_DIR)/StringLib.o \
+    $(OBJ_DIR)/TimeUtils.o \
+    $(OBJ_DIR)/Vector.o \
+    $(OBJ_DIR)/WinCThreads.o \
+    $(OBJ_DIR)/WinProcesses.o \
+    $(OBJ_DIR)/ZipLib.o \
+    $(OBJ_DIR)/miniz.o \
+
+INCLUDES := \
+    -Iinclude \
+
+LINKS := \
+
+DEFINES := \
+
+WARNINGS := \
+    -Wall \
+    -Wextra \
+    -Werror \
+
+all: $(OBJ_FILES)
+	$(ARCHIVE) $(OBJ_DIR)/cnext.a $(OBJ_FILES)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c $(INCLUDE_DIR)/%.h $(MAKEFILE) include.mk
+	$(MKDIR) $(OBJ_DIR)
+	$(CXX) $(FLAGS) $(INCLUDES) $(DEFINES) $(WARNINGS) -c $< -o $@
+
---
+++ lib/cnext/makefile
@@ -0,0 +1,6 @@
+BUILD_DIR   := build
+OBJ_DIR     := $(BUILD_DIR)/obj64
+CC          := gcc
+CXX         := g++
+
+include include.mk
