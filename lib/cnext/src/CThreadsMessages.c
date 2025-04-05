43473da25f0f7a31c9acecd4b0aeaeeff5d3018e
James B. Card <jbcard@gmail.com>
2025-04-05 08:40:36 -0700

    Updating CThreadsMessages.c to use new Messages library infrastructure.

--- lib/cnext/src/CThreadsMessages.c
+++ lib/cnext/src/CThreadsMessages.c
@@ -29,6 +29,7 @@
 /// @file
 
 #include "CThreadsMessages.h"
+#include "Trie.h"
 
 #if LOGGING_ENABLED && CTHREADS_LOGGING_ENABLED
 #include "LoggingLib.h"
@@ -39,24 +40,6 @@
 #define LOG_MALLOC_FAILURE(...) {}
 #endif
 
-/// @struct thrd_msg_q_t
-///
-/// @brief Definition for a thread's message queue.
-///
-/// @param head The head of the message queue.  Messages will be removed from
-///   this pointer.
-/// @param tail The tail of the message queue.  Messages will be added to this
-///   pointer.
-/// @param condition A condition (cnd_t) that will allow for signalling between
-///   threads when adding a message to the queue.
-/// @param lock A mutex (mtx_t) to guard the condition.
-typedef struct thrd_msg_q_t {
-  msg_t *head;
-  msg_t *tail;
-  cnd_t condition;
-  mtx_t lock;
-} thrd_msg_q_t;
-
 #ifdef __cplusplus
 extern "C"
 {
@@ -64,7 +47,7 @@ extern "C"
 
 void thrd_msg_q_storage_init(void);
 int thrd_msg_q_create(void);
-int thrd_msg_q_destroy(thrd_msg_q_t *queue);
+int thrd_msg_q_destroy(msg_q_t *queue);
 
 #ifdef __cplusplus
 }
@@ -73,21 +56,21 @@ int thrd_msg_q_destroy(thrd_msg_q_t *queue);
 /// @var message_queues
 ///
 /// @brief Root storage for the message queues.  Keys are thrd_t values.  Values
-/// are thrd_msg_q_t values.  There is one thrd_msg_q_t in the tree for ever
-/// thread that is currently running.  Queues are created when a thread starts
-/// and destroyed when it exits.
+/// are msg_q_t values.  There is one msg_q_t in the tree for ever thread that
+/// is currently running.  Queues are created when a thread starts and destroyed
+/// when it exits.
 Trie *message_queues = NULL;
 
-/// @fn thrd_msg_q_t* get_thread_thrd_msg_q(void)
+/// @fn msg_q_t* get_thread_thrd_msg_q(void)
 ///
 /// @brief Get the message queue for the current thread.
 ///
-/// @return Returns a pointer to the thrd_msg_q_t for the thread making this
-/// call.  This should always be a non-NULL value unless something is wrong with
-/// the system.
-static inline thrd_msg_q_t* get_thread_thrd_msg_q(void) {
+/// @return Returns a pointer to the msg_q_t for the thread making this call.
+/// This should always be a non-NULL value unless something is wrong with the
+/// system.
+static inline msg_q_t* get_thread_thrd_msg_q(void) {
   thrd_t thr = thrd_current();
-  return (thrd_msg_q_t*) trieGetValue(message_queues, &thr, sizeof(thr));
+  return (msg_q_t*) trieGetValue(message_queues, &thr, sizeof(thr));
 }
 
 /// @var thrd_msg_q_storage_initialized
@@ -117,14 +100,14 @@ void thrd_msg_q_storage_init(void) {
 
 /// @fn int thrd_msg_q_create(void)
 ///
-/// @brief Dynamically allocate and initialize a thrd_msg_q_t that will server as the
+/// @brief Dynamically allocate and initialize a msg_q_t that will serve as the
 /// message queue for the current thread and associate it with this thread's
 /// thrd_t value in the message_queues radix tree.
 ///
 /// @return Returns thrd_success on success, thrd_error on failure.
 int thrd_msg_q_create(void) {
   int return_value = thrd_success;
-  thrd_msg_q_t *old_queue = NULL;
+  msg_q_t *old_queue = NULL;
   
   if (get_thread_thrd_msg_q() != NULL) {
     // This should be impossible, but already have a queue for this thread.
@@ -133,27 +116,13 @@ int thrd_msg_q_create(void) {
   }
   
   thrd_t thr = thrd_current();
-  thrd_msg_q_t *new_queue = (thrd_msg_q_t*) calloc(1, sizeof(thrd_msg_q_t));
+  msg_q_t *new_queue = msg_q_create(NULL, MSG_THRD_SAFE, NULL, NULL);
   if (new_queue == NULL) {
     goto new_queue_calloc_failure;
   }
   
-  // new_queue->head and new_queue->tail are initialized to NULL by the
-  // calloc call.
-  if (cnd_init(&new_queue->condition) != thrd_success) {
-    // Can't proceed.  Bail.
-    goto cnd_init_failure;
-  }
-  
-  if (mtx_init(&new_queue->lock, mtx_plain | mtx_timed)
-    != thrd_success
-  ) {
-    // Can't proceed.  Bail.
-    goto mtx_init_failure;
-  }
-  
   old_queue =
-    (thrd_msg_q_t*) trieSetValue(message_queues, &thr, sizeof(thr), new_queue);
+    (msg_q_t*) trieSetValue(message_queues, &thr, sizeof(thr), new_queue);
   if (old_queue != NULL) {
     // This shouldn't be possible, but we have to destroy the old queue.
     thrd_msg_q_destroy(old_queue);
@@ -161,17 +130,13 @@ int thrd_msg_q_create(void) {
   
   return return_value; // thrd_success
   
-mtx_init_failure:
-  cnd_destroy(&new_queue->condition);
-cnd_init_failure:
-  free(new_queue); new_queue = NULL;
 new_queue_calloc_failure:
   return_value = thrd_error;
   
   return return_value; // thrd_error
 }
 
-/// @fn int thrd_msg_q_destroy(thrd_msg_q_t *queue)
+/// @fn int thrd_msg_q_destroy(msg_q_t *queue)
 ///
 /// @brief Destroy the specified message queue.  If none is specifiied, remove
 /// the queue for the current thread and destroy it.
@@ -180,7 +145,7 @@ new_queue_calloc_failure:
 /// for the current thread will be destroyed.
 ///
 /// @return Returns thrd_success on success, thrd_error on failure.
-int thrd_msg_q_destroy(thrd_msg_q_t *queue) {
+int thrd_msg_q_destroy(msg_q_t *queue) {
   int return_value = thrd_success;
   thrd_t thr = thrd_current();
   
@@ -189,7 +154,7 @@ int thrd_msg_q_destroy(thrd_msg_q_t *queue) {
   // deleting it.
   if (queue == NULL) {
     queue =
-      (thrd_msg_q_t*) trieSetValue(message_queues, &thr, sizeof(thr), NULL);
+      (msg_q_t*) trieSetValue(message_queues, &thr, sizeof(thr), NULL);
     
     // Delete the node storage for the queue in the radix tree.
     trieDeleteValue(message_queues, &thr, sizeof(thr));
@@ -200,16 +165,8 @@ int thrd_msg_q_destroy(thrd_msg_q_t *queue) {
       return return_value; // thrd_success
     }
   }
-  
-  for (msg_t *cur = queue->head; cur != NULL; ) {
-    msg_t *next = cur->next;
-    msg_destroy(cur);
-    cur = next;
-  }
-  
-  mtx_destroy(&queue->lock);
-  cnd_destroy(&queue->condition);
-  free(queue); queue = NULL;
+
+  return_value = msg_q_destroy(queue);
   
   return return_value;
 }
@@ -221,14 +178,8 @@ int thrd_msg_q_destroy(thrd_msg_q_t *queue) {
 ///
 /// @return Returns the head of the message queue, which may be NULL.
 msg_t* thrd_msg_q_peek(void) {
-  msg_t *head = NULL;
-  
-  thrd_msg_q_t *queue = get_thread_thrd_msg_q();
-  if (queue != NULL) {
-    head = queue->head;
-  }
-  
-  return head;
+  msg_q_t *queue = get_thread_thrd_msg_q();
+  return msg_q_peek(queue);
 }
 
 /// @fn msg_t* thrd_msg_q_peek(void)
@@ -237,28 +188,8 @@ msg_t* thrd_msg_q_peek(void) {
 ///
 /// @return Returns the head of the message queue, which may be NULL.
 msg_t* thrd_msg_q_pop(void) {
-  thrd_msg_q_t *queue = get_thread_thrd_msg_q();
-  msg_t *head = NULL;
-  
-  if ((queue == NULL) || (mtx_lock(&queue->lock) != thrd_success)) {
-    // Error case.
-    return head; // NULL
-  }
-  
-  head = queue->head;
-  if (head != NULL) {
-    queue->head = head->next;
-    head->next = NULL;
-    
-    if (queue->head == NULL) {
-      // Empty queue.  Set queue->tail to NULL too.
-      queue->tail = NULL;
-    }
-  }
-  
-  mtx_unlock(&queue->lock);
-  
-  return head;
+  msg_q_t *queue = get_thread_thrd_msg_q();
+  return msg_q_pop(queue);
 }
 
 /// @fn msg_t* thrd_msg_q_pop_type(int type)
@@ -271,129 +202,14 @@ msg_t* thrd_msg_q_pop(void) {
 /// @return Returns the first message of the queue with the specified type on
 /// success, NULL on failure.
 msg_t* thrd_msg_q_pop_type(int type) {
-  thrd_msg_q_t *queue = get_thread_thrd_msg_q();
-  msg_t *return_value = NULL;
-  msg_t *prev = NULL;
-  msg_t *cur = queue->head;
-  msg_t **prev_next = &queue->head;
-  
-  if (mtx_lock(&queue->lock) != thrd_success) {
-    // Error case.
-    return return_value; // NULL
-  }
-  
-  while ((cur != NULL) && (cur->type != type)) {
-    prev = cur;
-    prev_next = &cur->next;
-    cur = cur->next;
-  }
-  
-  if (cur != NULL) {
-    // Desired type was found.  Remove the message from the queue.
-    return_value = cur;
-    *prev_next = cur->next;
-    
-    if (queue->head == NULL) {
-      // Empty queue.  Set queue->tail to NULL too.
-      queue->tail = NULL;
-    }
-    if (queue->tail == cur) {
-      queue->tail = prev;
-    }
-    cur->next = NULL;
-  }
-  
-  mtx_unlock(&queue->lock);
-  
-  return return_value;
+  msg_q_t *queue = get_thread_thrd_msg_q();
+  return msg_q_pop_type(queue, type);
 }
 
-/// @fn msg_t* thrd_msg_q_wait_for_type_(int *type, const struct timespec *ts)
-///
-/// @brief Wait for a message of a given type to be available in the message
-/// queue or until a specified time has elapsed.  Remove the message from the
-/// queue and return it if one is available before the specified time is
-/// reached.
-///
-/// @param type A pointer to the message type to look for.  If this parameter is
-///   NULL then the first message of any type will be returned.
-/// @param ts A pointer to a struct timespec that specifies the end of the time
-///   period to wait for.  If this parameter is NULL then an infinite timeout
-///   will be used.
-///
-/// @return Returns the first message of the provided type if one is available
-/// before the specified time.  Returns NULL if no such message is available
-/// within that time period or if an error occurrs.
-msg_t* thrd_msg_q_wait_for_type_(int *type, const struct timespec *ts) {
-  thrd_msg_q_t *queue = get_thread_thrd_msg_q();
-  msg_t *return_value = NULL;
-  msg_t *prev = NULL;
-  msg_t *cur = queue->head;
-  msg_t **prev_next = &queue->head;
-  int lock_status = thrd_success;
-  int wait_status = thrd_success;
-  int search_type = 0;
-  if (type != NULL) {
-    // This saves us from having to dereference the pointer on each iteration
-    // of the loop below.
-    search_type = *type;
-  }
-  
-  if (ts == NULL) {
-    lock_status = mtx_lock(&queue->lock);
-  } else {
-    lock_status = mtx_timedlock(&queue->lock, ts);
-  }
-  if (lock_status != thrd_success) {
-    // Error case.
-    return return_value; // NULL
-  }
-  // mtx_timedlock will return thrd_timedout if the timeout is reached, so we'll
-  // never reach this point if we've exceeded our timeout.
-  
-  while (return_value == NULL) {
-    while ((cur != NULL) && (type != NULL) && (cur->type != search_type)) {
-      prev = cur;
-      prev_next = &cur->next;
-      cur = cur->next;
-    }
-    
-    if (cur != NULL) {
-      // Desired type was found.  Remove the message from the queue.
-      return_value = cur;
-      *prev_next = cur->next;
-      
-      if (queue->head == NULL) {
-        // Empty queue.  Set queue->tail to NULL too.
-        queue->tail = NULL;
-      }
-      if (queue->tail == cur) {
-        queue->tail = prev;
-      }
-      cur->next = NULL;
-    } else {
-      // Desired type was not found.  Block until something else is pushed.
-      if (ts == NULL) {
-        wait_status = cnd_wait(&queue->condition, &queue->lock);
-      } else {
-        wait_status = cnd_timedwait(&queue->condition, &queue->lock, ts);
-      }
-      if (wait_status != thrd_success) {
-        break;
-      }
-      // cnd_timedwait will return thrd_timedout if the timeout is reached, so
-      // we won't continue the loop if we've exceeded our timeout.
-    }
-    
-    prev = NULL;
-    cur = queue->head;
-    prev_next = &queue->head;
-  }
-  
-  mtx_unlock(&queue->lock);
-  
-  return return_value;
-}
+// Underlying worker function in Messages.c.  Expose the prototype here so that
+// we can bypass a layer of abstraction into the Messages library.
+msg_t* msg_q_wait_for_type_(msg_q_t *queue, int *type,
+  const struct timespec *ts);
 
 /// @fn msg_t* thrd_msg_q_wait(const struct timespec *ts)
 ///
@@ -409,7 +225,8 @@ msg_t* thrd_msg_q_wait_for_type_(int *type, const struct timespec *ts) {
 /// the specified time.  Returns NULL if no such message is available within
 /// that time period or if an error occurrs.
 msg_t* thrd_msg_q_wait(const struct timespec *ts) {
-  return thrd_msg_q_wait_for_type_(NULL, ts);
+  msg_q_t *queue = get_thread_thrd_msg_q();
+  return msg_q_wait_for_type_(queue, NULL, ts);
 }
 
 /// @fn msg_t* thrd_msg_q_wait_for_type(int type, const struct timespec *ts)
@@ -417,7 +234,6 @@ msg_t* thrd_msg_q_wait(const struct timespec *ts) {
 /// @brief Wait until there is a message of a specified type in the queue or
 /// until a specified has been reached.  If there is a message of the specified
 /// type in the queue before the timeout, pop it from the queue.
-///
 ///
 /// @param type The message type to look for.
 /// @param ts A pointer to a struct timespec that specifies the end of the time
@@ -428,7 +244,8 @@ msg_t* thrd_msg_q_wait(const struct timespec *ts) {
 /// before the specified time.  Returns NULL if no such message is available
 /// within that time period or if an error occurrs.
 msg_t* thrd_msg_q_wait_for_type(int type, const struct timespec *ts) {
-  return thrd_msg_q_wait_for_type_(&type, ts);
+  msg_q_t *queue = get_thread_thrd_msg_q();
+  return msg_q_wait_for_type_(queue, &type, ts);
 }
 
 
@@ -449,163 +266,16 @@ int thrd_msg_q_push(thrd_t thr, msg_t *msg) {
     return return_value; // thrd_error
   }
   
-  thrd_msg_q_t *queue
-    = (thrd_msg_q_t*) trieGetValue(message_queues, &thr, sizeof(thr));
+  msg_q_t *queue
+    = (msg_q_t*) trieGetValue(message_queues, &thr, sizeof(thr));
   if (queue == NULL) {
     // Destination thread has exited.  Fail.
     return return_value; // thrd_error
   }
   
-  if (mtx_lock(&queue->lock) != thrd_success) {
-    // Error case.
-    return return_value; // thrd_error
-  }
-  
   msg->from.thrd = thrd_current();
   msg->to.thrd = thr;
-  msg->next = NULL;
-  if (queue->tail != NULL) {
-    queue->tail->next = msg;
-    queue->tail = msg;
-  } else {
-    // Empty queue.  Populate both queue->head and queue->tail.
-    queue->head = msg;
-    queue->tail = msg;
-  }
-  msg->endpoint_type = MESSAGE_ENDPOINT_TYPE_THREAD;
   
-  // Let all the waiters know that there's something new in the queue now.
-  return_value = cnd_broadcast(&queue->condition);
-  
-  mtx_unlock(&queue->lock);
-  
-  return return_value;
-}
-
-/// @fn msg_t* msg_wait_for_reply_with_type_thrd(
-///   msg_t *sent, bool release, int *type, const struct timespec *ts)
-///
-/// @brief Wait for a reply from the thread recipient of a message.
-///
-/// @param sent The message that was originally sent to the recipient.
-/// @param release Whether or not the provided sent message should be released
-///   (*NOT* destroyed) after the recipient has indicated that they're done
-///   processing our sent message.
-/// @param type A pointer to an integer type of message that the caller is
-///   waiting for.  If this parameter is NULL, no type will be considered.
-/// @param ts A pointer to a struct timespec that holds the end time to wait
-///   until for a reply.  If this parameter is NULL, then an infinite timeout
-///   will be used.
-///
-/// @return Returns a pointer to the msg_t received from the recipient of
-/// the original message on success, NULL on failure or if the provided timeout
-/// time is reached.
-msg_t* msg_wait_for_reply_with_type_thrd(
-  msg_t *sent, bool release,
-  int *type, const struct timespec *ts
-) {
-  msg_t *reply = NULL;
-
-  thrd_msg_q_t *queue = get_thread_thrd_msg_q();
-  if (queue == NULL) {
-    // Something is wrong.  Bail.
-    return reply; // NULL
-  }
-
-  if (sent == NULL) {
-    // Invalid.
-    return reply; // NULL
-  }
-
-  // We need to grab the original recipient of the message that was sent before
-  // we wait for done in case the recipient reuses this message as the reply.
-  thrd_t recipient = sent->to.thrd;
-
-  if (msg_wait_for_done(sent, ts) != msg_success) {
-    // Invalid state of the message.  Fail.
-    return reply; // NULL
-  }
-
-  if (release == true) {
-    // We're done with the message that was originally sent and the caller has
-    // indicated that it is to be released now.
-    msg_release(sent);
-  }
-
-  // Recipient has processed the message.  We now need to wait for their reply.
-  int lock_status = msg_success;
-  if (ts == NULL) {
-    lock_status = mtx_lock(&queue->lock);
-  } else {
-    lock_status = mtx_timedlock(&queue->lock, ts);
-  }
-  if (lock_status != msg_success) {
-    // Either we've timed out or there's a problem with the lock.  Either way,
-    // we're done.  Bail.
-    return reply; // NULL
-  }
-
-  // mtx_timedlock will return thrd_timedout if the timeout is
-  // reached, so we'll never reach this point if we've exceeded our timeout.
-  msg_t *prev = NULL;
-  msg_t *cur = queue->head;
-  msg_t **prev_next = &queue->head;
-  int searchType = 0;
-  if (type != NULL) {
-    // This saves us from having to dereference the pointer in every iteration
-    // of the loop below.
-    searchType = *type;
-  }
-
-  // Enter our main wait loop.
-  int wait_status = msg_success;
-  while (reply == NULL) {
-    while ((cur != NULL)
-      && ((cur->from.thrd != recipient)
-        || ((type != NULL) && (cur->type != searchType))
-      )
-    ) {
-      prev = cur;
-      prev_next = &cur->next;
-      cur = cur->next;
-    }
-
-    if (cur != NULL) {
-      // Desired reply was found.  Remove the message from the thread.
-      reply = cur;
-      *prev_next = cur->next;
-
-      if (queue->head == NULL) {
-        // Empty queue.  Set queue->tail to NULL too.
-        queue->tail = NULL;
-      }
-      if (queue->tail == cur) {
-        queue->tail = prev;
-      }
-      cur->next = NULL;
-    } else {
-      // Desired reply was not found.  Block until something else is pushed.
-      if (ts == NULL) {
-        wait_status = cnd_wait(&queue->condition, &queue->lock);
-      } else {
-        wait_status = cnd_timedwait(
-          &queue->condition, &queue->lock, ts);
-      }
-      if (wait_status != msg_success) {
-        // Something isn't as expected.  Bail.
-        break;
-      }
-      // cnd_timedwait will return thrd_timedout if the timeout is
-      // reached, so we won't continue the loop if we've exceeded our timeout.
-    }
-
-    prev = NULL;
-    cur = queue->head;
-    prev_next = &queue->head;
-  }
-
-  mtx_unlock(&queue->lock);
-
-  return reply;
+  return msg_q_push(queue, msg);
 }
 
